457bc9f1d3cef57953c3c5eedba5fdb9b675fbfa
Andy Wingo <wingo@pobox.com>
2020-05-05 22:15:46 +0200

    Update (system base compile) header
    * module/system/base/compile.scm (system): Clean up imports.

--- module/system/base/compile.scm
+++ module/system/base/compile.scm
@@ -1,30 +1,25 @@
 ;;; High-level compiler interface
 
-;; Copyright (C) 2001, 2009, 2010, 2011, 2012, 2013 Free Software Foundation, Inc.
+;; Copyright (C) 2001,2005,2008-2013,2016,2020 Free Software Foundation, Inc.
 
-;;; This library is free software; you can redistribute it and/or
-;;; modify it under the terms of the GNU Lesser General Public
-;;; License as published by the Free Software Foundation; either
-;;; version 3 of the License, or (at your option) any later version.
+;;; This library is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU Lesser General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
 ;;;
-;;; This library is distributed in the hope that it will be useful,
-;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-;;; Lesser General Public License for more details.
+;;; This library is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+;;; General Public License for more details.
 ;;;
-;;; You should have received a copy of the GNU Lesser General Public
-;;; License along with this library; if not, write to the Free Software
-;;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+;;; You should have received a copy of the GNU Lesser General Public License
+;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Code:
 
 (define-module (system base compile)
-  #:use-module (system base syntax)
   #:use-module (system base language)
   #:use-module (system base message)
-  #:use-module (system vm vm) ;; FIXME: there's a reason for this, can't remember why tho
-  #:use-module (ice-9 regex)
-  #:use-module (ice-9 optargs)
   #:use-module (ice-9 receive)
   #:export (compiled-file-name
             compile-file
